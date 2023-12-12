6c1a2c07396b0604b3186d23ec20f15b45dc9550
ZiyaoLi <leeeezy@126.com>
2023-12-12 07:11:06 +0000

    add installing

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+TORCH=1.12.1
+CUDA=cu116
+
+pip install torch-scatter     -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-sparse      -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-cluster     -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-geometric
